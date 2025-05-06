d4b2fec38b9fed7b5d1d62e2249c05d30a84095b
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-06 17:01:04 +0600

    NavItem done

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,16 +1,17 @@
+"use client"
 import { headerData } from '@/constants/data'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function HeaderMenu() {
-
     const pathName = usePathname()
+    console.log(pathName)
   return (
     <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-LightColor'>
      {headerData.map((item) => (
-      <Link className={`hover:text-shop_light_green hoverEffect relative group`} key={item?.title} href={item?.href}>{item?.title}<span className={`absolute -bottom-0.5 left-1/5 h-0.5 w-0 bg-shop_light_green  group-hover:w-1/2 hoverEffect group-hover:left-0`}/>
-      <span className={`absolute -bottom-0.5 right-1/5 h-0.5 w-0 bg-shop_light_green  group-hover:w-1/2 hoverEffect group-hover:right-0`}/></Link>
+      <Link className={`hover:text-shop_light_green hoverEffect relative group ${pathName === item?.href &&'text-shop_light_green' }`} key={item?.title} href={item?.href}>{item?.title}<span className={`absolute -bottom-0.5 left-1/2 h-0.5 w-0 bg-shop_light_green  group-hover:w-1/2 hoverEffect group-hover:left-0 ${pathName === item?.href && "w-1/2"}`}/>
+      <span className={`absolute -bottom-0.5 right-1/2 h-0.5 w-0 bg-shop_light_green  group-hover:w-1/2 hoverEffect group-hover:right-0 ${pathName === item?.href && "w-1/2"}`}/></Link>
      ))}
     </div>
   )
