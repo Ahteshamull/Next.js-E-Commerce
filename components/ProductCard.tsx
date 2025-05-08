1b8ce269774ce41de6ee52e5f5e721f97cec1979
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-08 17:36:51 +0600

    product show done

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -18,15 +18,15 @@ const ProductCard = ({ product }: { product: Product }) => {
       <div className="relative group overflow-hidden bg-shop_light_bg">
         {product?.images && (
           <Link href={`/product/${product?.slug?.current}`}>
-            {/* <Image
-              src={urlFor(product.images[0]).url()}
+            <Image
+              src={urlFor(product?.images[1]).url()}
               alt="productImage"
               width={500}
               height={500}
               priority
               className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
               ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
-            /> */}
+            />
           </Link>
         )}
         <ProductSideMenu product={product} />
