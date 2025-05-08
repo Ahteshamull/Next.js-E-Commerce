cc37a90c148af200fcba0cfa4bbcfede2e0c39cf
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-08 12:47:08 +0600

    sanity add done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,8 @@
-import type { Metadata } from "next";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - E@Store",
-    default: "E@Store",
-  },
-  description: "E@Store Online Shop",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-sans">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-sans"> {children}</body>
+    </html>
   );
-}
+};
+export default RootLayout;
