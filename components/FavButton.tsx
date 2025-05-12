7f43dc3181c5cece3dbb7d0f5b6ee7025c206383
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-12 13:27:42 +0600

    cart and wishlist product done

--- components/FavButton.tsx
+++ components/FavButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function FavButton() {
   return (
-    <Link className='group relative' href={'/cart'}>
+    <Link className='group relative' href={'/wishlist'}>
       <Heart className='w-5 h-5 hover:text-shop_light_green hoverEffect'/>
       <span className='absolute -top-2 -right-2 text-xs w-4 h-4 flex items-center justify-center text-white bg-shop_light_green rounded-full '>
         0
